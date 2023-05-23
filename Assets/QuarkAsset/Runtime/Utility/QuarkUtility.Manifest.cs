c05c6fce374a58fe72dccf0e97abd159f1e957f7
Donn <44013616+DonnYep@users.noreply.github.com>
2023-05-23 21:40:21 +0800

    Update: 更改manifest序列化为utility工具类

---
+++ Assets/QuarkAsset/Runtime/Utility/QuarkUtility.Manifest.cs
@@ -0,0 +1,63 @@
+﻿using Quark.Asset;
+
+namespace Quark
+{
+    public partial class QuarkUtility
+    {
+        public class Manifest
+        {
+            public static string Serialize(QuarkManifest manifest, string aesKey)
+            {
+                var aesKeyBytes = QuarkUtility.GenerateBytesAESKey(aesKey);
+                return Serialize(manifest, aesKeyBytes);
+            }
+            public static string Serialize(QuarkManifest manifest, byte[] aesKeyBytes)
+            {
+                var manifestJson = QuarkUtility.ToJson(manifest);
+                var encrypt = aesKeyBytes != null && aesKeyBytes.Length > 0;
+                string serializedContext = string.Empty;
+                if (encrypt)
+                    serializedContext = QuarkUtility.AESEncryptStringToString(manifestJson, aesKeyBytes);
+                else
+                    serializedContext = manifestJson;
+                return serializedContext;
+            }
+            /// <summary>
+            /// 使用配置的aesKey反序列化manifest；
+            /// </summary>
+            /// <param name="manifestContext">读取到的文本内容</param>
+            /// <returns>反序列化后的内容</returns>
+            public static QuarkManifest Deserialize(string manifestContext)
+            {
+                return Deserialize(manifestContext, QuarkDataProxy.QuarkAesEncryptionKey);
+            }
+            /// <summary>
+            /// 使用自定义的aesKey反序列化manifest；
+            /// </summary>
+            /// <param name="manifestContext">读取到的文本内容</param>
+            /// <param name="aesKey">对称加密密钥</param>
+            /// <returns>反序列化后的内容</returns>
+            public static QuarkManifest Deserialize(string manifestContext, string aesKey)
+            {
+                var aesKeyBytes = QuarkUtility.GenerateBytesAESKey(aesKey);
+                return Deserialize(manifestContext, aesKeyBytes);
+            }
+            public static QuarkManifest Deserialize(string manifestContext, byte[] aesKeyBytes)
+            {
+                QuarkManifest quarkAssetManifest = null;
+                try
+                {
+                    var encrypted = aesKeyBytes.Length > 0 ? true : false;
+                    var unencryptedManifest = manifestContext;
+                    if (encrypted)
+                    {
+                        unencryptedManifest = QuarkUtility.AESDecryptStringToString(manifestContext, aesKeyBytes);
+                    }
+                    quarkAssetManifest = QuarkUtility.ToObject<QuarkManifest>(unencryptedManifest);
+                }
+                catch { }
+                return quarkAssetManifest;
+            }
+        }
+    }
+}
--- Assets/QuarkAsset/Runtime/Utility/QuarkUtility.cs
+++ Assets/QuarkAsset/Runtime/Utility/QuarkUtility.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
 using System.Collections.Generic;
